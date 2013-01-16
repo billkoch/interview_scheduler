456af3fa309ce737650f9f3924a02fa4a20b028f
Mike Derrick <mike33d@gmail.com>
2013-01-16 13:05:06 -0500

    Added authorization to the admin controllers and an authorize before_filter to manage permissions

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,27 @@
+class HomeController < ApplicationController
+  skip_before_filter :authorize
+  def index
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def admin
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def login
+    signin_params = params['admin']
+    if signin_params['pass'].eql?('nu!tdcr3g') and signin_params['user'].eql?('lomison')
+      session["password"] = signin_params["pass"]
+    end
+    redirect_to home_index_url
+  end
+
+  def logout
+    session["password"] = nil
+    redirect_to home_index_url
+  end
+end
